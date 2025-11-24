c4abece24b9e1d57810e5bd72f01229706c1b43b
Francesc Català-Moll <70643921+xec-cm@users.noreply.github.com>
2025-11-24 09:19:31 +0100

    feat: setup docker deployment pipeline and fix footer links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://francesccatala.com',
+  site: 'https://francesc-catala.com',
   vite: {
     plugins: [tailwindcss()]
   }
