c417f6b8a4838007b2ca2777e39a918bc362ea2f
Francesc Català-Moll <70643921+xec-cm@users.noreply.github.com>
2026-01-10 22:38:47 +0100

    fix: indexation issues (trailing slash and canonicals)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://francesc-catala.com',
+  trailingSlash: 'always',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
